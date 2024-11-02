21bd44b7339e65b814545ade9abb18fce99249ef
Agustina Quinteros <21agustinaa@gmail.com>
2024-11-02 15:59:28 -0300

    agrego cambios

--- entrega2/js/Tablero.js
+++ entrega2/js/Tablero.js
@@ -60,6 +60,17 @@ class Tablero {
             dropArea.draw();
         })
     }
+    //nuevo
+    drawDropAreas() {
+    this.casillasDrop.forEach(dropArea => {
+        dropArea.draw(ctx); 
+
+        // Dibuja la flecha en el área de drop
+        const centerX = dropArea.posx + (dropArea.width / 2);
+        const centerY = dropArea.posy + (dropArea.width / 2);
+        this.dibujarFlecha(ctx, centerX, centerY); 
+    });
+}
 
     getLowerCasillaByIndex(col){
         let fila = this.MAXFILAS-1;
@@ -118,5 +129,29 @@ class Tablero {
             this.casillas.push(filaCasillas);
         }
             */
+    }
+
+    dibujarFlecha(ctx, x, y) {
+        ctx.save();
+        ctx.translate(x, y);
+        ctx.rotate(-Math.PI / 2);
+        
+        // Dibuja la línea de la flecha
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(0, -20); // Cambia la longitud de la flecha según sea necesario
+        ctx.strokeStyle = "white"; // Color de la línea
+        ctx.lineWidth = 2;
+        ctx.stroke();
+
+        // Dibuja la punta de la flecha
+        ctx.beginPath();
+        ctx.moveTo(-5, -15); // Ajusta la posición de la punta
+        ctx.lineTo(0, -25);
+        ctx.lineTo(5, -15);
+        ctx.fillStyle = "white"; // Color de la punta
+        ctx.fill();
+
+        ctx.restore();
     }
 }
