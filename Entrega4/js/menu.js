a3bba449d948134796092bb48be3b29526261335
Agustina Quinteros <21agustinaa@gmail.com>
2024-11-18 18:51:50 -0300

    inciso 6 realizado

--- Entrega4/js/menu.js
+++ Entrega4/js/menu.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+    //[OPCIONAL] Punto 1
     const header = document.getElementById('header');
     const loader = document.getElementById('loader');
     const content = document.querySelector('.indexLoad');
@@ -9,7 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const loadingInterval = setInterval(() => {
         progress++;
 
-        // Calcular la imagen que debe mostrarse
         const imageIndex = Math.floor((progress / 100) * totalImages);
         progressImage.src = `../images/${imageIndex}.png`;
 
@@ -24,9 +24,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-
-
-
 function toggleMenu() {
     const menuLogo = document.getElementById('logo-menu');
     if (menuLogo) {
@@ -36,8 +33,6 @@ function toggleMenu() {
         } else {
             menuLogo.src = '../images/logoMenu.png';
         }
-    } else {
-        console.error('Elemento con id "menu-logo" no encontrado');
     }
 }
 
@@ -48,7 +43,7 @@ window.addEventListener('scroll', function () {
         logo.style.width = '150px'; 
         logo.style.height = '86px';
         logo.style.marginTop='0px';
-    } else if (window.scrollY === 0) {
+    } else if (window.scrollY === 0) { //si el scroll es igual a cero, o sea esta al principio de la pagina, el logo vuelve a su tamaño inical
         logo.style.width = '560px';
         logo.style.height = '320px';
         logo.style.marginTop='75px';
@@ -69,6 +64,28 @@ window.addEventListener('scroll', function () {
     }
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    const images = ["../images/cardGrande.png", "../images/imgCard1.png", "../images/imgCard2.png", "../images/imgCard3.png"]; // Array de imágenes
+    const cardImage = document.getElementById("card-image");
+    let currentIndex = 0;
+
+    const changeImage = () => {
+        //aplica el rebote en la card 
+        cardImage.classList.add("bounce");
+
+        //cambian la imagen de la card
+        setTimeout(() => {
+            currentIndex = (currentIndex + 1) % images.length;
+            cardImage.src = images[currentIndex];
+
+            //se eliminar el efecto del rebote para agregarselo a la nueva imagen
+            cardImage.classList.remove("bounce");
+        }, 500); // Duración de la animación
+    };
+
+    // cambia la imagen cada 3 segundos - Punto 6
+    setInterval(changeImage, 3000);
+});
 
 
 
