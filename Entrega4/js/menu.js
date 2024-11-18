ab4448d731deb5c8a52c78aa35f69482f1464c9e
Agustina Quinteros <21agustinaa@gmail.com>
2024-11-18 15:44:10 -0300

    agrego nuevos cambios en mi seccion

---
+++ Entrega4/js/menu.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Obtener los elementos
+    const loader = document.getElementById('loader');
+    const content = document.querySelector('.indexLoad');
+    const progressImage = document.getElementById('progress'); 
+    let progress = 0;
+    const totalImages = 11;
+    content.style.display='none';
+    const loadingInterval = setInterval(() => {
+        progress++;
+
+        // Calcular la imagen que debe mostrarse
+        const imageIndex = Math.floor((progress / 100) * totalImages);
+        progressImage.src = `../images/${imageIndex}.png`;
+
+        if (progress >= 100) {
+            clearInterval(loadingInterval);
+            loader.style.display = 'none';
+            content.style.display = 'block';
+        }
+    }, 90); 
+});
+
+
+
+
+function toggleMenu() {
+    const menuLogo = document.getElementById('logo-menu');
+    if (menuLogo) {
+        const currentSrc = menuLogo.src;
+        if (currentSrc.includes('logoMenu.png')) {
+            menuLogo.src = '../images/logoCruz.png';
+        } else {
+            menuLogo.src = '../images/logoMenu.png';
+        }
+    } else {
+        console.error('Elemento con id "menu-logo" no encontrado');
+    }
+}
+
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('header');
+    const logo = document.getElementById('logo');
+    if (window.scrollY > 50) {
+        logo.style.width = '150px'; 
+        logo.style.height = '86px';
+        logo.style.marginTop='0px';
+    } else if (window.scrollY === 0) {
+        logo.style.width = '560px';
+        logo.style.height = '320px';
+        logo.style.marginTop='75px';
+    }
+  });
+
+  window.addEventListener('load', function () {
+    const header = document.getElementById('header');
+    const logo = document.getElementById('logo');
+    if (window.scrollY > 50) {
+        logo.style.width = '150px';  
+        logo.style.height = '86px';
+        logo.style.marginTop='0px';
+    } else if (window.scrollY === 0) {
+        logo.style.width = '560px';
+        logo.style.height = '320px';
+        logo.style.marginTop='75px';
+    }
+});
+
+
+
+//container1
+
+
